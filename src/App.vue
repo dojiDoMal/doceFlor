2c24a2a24390c42a31887e161fa5a33e8400a90b
dojiDoMal <andersonamacedo@hotmail.com>
2021-08-04 22:44:47 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <Header/>
+    </v-app-bar>
+
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
